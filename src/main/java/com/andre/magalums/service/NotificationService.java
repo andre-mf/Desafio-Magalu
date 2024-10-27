74e773fa48c967cb56e5f1a0970ec250f4e2f652
andre-mf <andremendesfelix@gmail.com>
2024-10-26 23:55:36 -0300

    Criar serviço de consulta de notificações disponíveis para envio, mock de envio de notificação com logging, serviço que atualiza o status da notificação e teste do fluxo completo (api → agendamento → rotina → envia notificacao → atualiza base)

--- src/main/java/com/andre/magalums/service/NotificationService.java
+++ src/main/java/com/andre/magalums/service/NotificationService.java
@@ -6,7 +6,10 @@ import com.andre.magalums.entity.Status;
 import com.andre.magalums.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class NotificationService {
@@ -32,5 +35,22 @@ public class NotificationService {
             notification.get().setStatus(Status.Values.CANCELLED.toStatus());
             notificationRepository.save(notification.get());
         }
+    }
+
+    public void checkAndSend(LocalDateTime dateTime) {
+        List<Notification> notifications = notificationRepository.findByStatusInAndDateTimeBefore(List.of(
+                Status.Values.PENDING.toStatus(),
+                Status.Values.ERROR.toStatus()
+        ), dateTime);
+        notifications.forEach(sendNotification());
+    }
+
+    private  Consumer<Notification> sendNotification() {
+        return notification -> {
+            // TODO - realizar o envio da notificação
+
+            notification.setStatus(Status.Values.SUCCESS.toStatus());
+            notificationRepository.save(notification);
+        };
     }
 }
